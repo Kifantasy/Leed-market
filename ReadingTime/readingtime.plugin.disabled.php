2ef46eb113423d5fa025a54c43111a9b31ce8e20
Simounet <contact@simounet.net>
2014-11-13 23:37:00 +0100

    ReadingTime : indentation

--- ReadingTime/readingtime.plugin.disabled.php
+++ ReadingTime/readingtime.plugin.disabled.php
@@ -15,17 +15,17 @@ function readingtime(&$event) {
     $filtered_content = ($configurationManager->get('articleDisplayMode') == 'summary') ? $event->getDescription() : $event->getContent();
 
     if( $filtered_content ) {
-		$readingtime =null;
-	    $word = str_word_count(strip_tags($filtered_content));
+        $readingtime =null;
+        $word = str_word_count(strip_tags($filtered_content));
         $minutes_estimate = floor($word / 200);
         $seconds_estimate = floor($word / (200 / 60));
-		$hours = intval($seconds_estimate / 3600);
-		$minutes=intval(($seconds_estimate % 3600) / 60);
-		$secondes=intval((($seconds_estimate % 3600) % 60));
-		if($hours>=1) { $readingtime .= $hours.'h ';}
-		if($minutes>=1) { $readingtime .= $minutes.'min ';}
-		if($minutes<1 && $hours == 0) { $readingtime .= $secondes.'s ';}
-		echo _t('READING_TIME').' '.$readingtime;
+        $hours = intval($seconds_estimate / 3600);
+        $minutes=intval(($seconds_estimate % 3600) / 60);
+        $secondes=intval((($seconds_estimate % 3600) % 60));
+        if($hours>=1) { $readingtime .= $hours.'h ';}
+        if($minutes>=1) { $readingtime .= $minutes.'min ';}
+        if($minutes<1 && $hours == 0) { $readingtime .= $secondes.'s ';}
+        echo _t('READING_TIME').' '.$readingtime;
     }
 }
 
