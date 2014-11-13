337b419b1caba171f9cda6a6a13b6441e1b1b445
Simounet <contact@simounet.net>
2014-11-13 23:36:00 +0100

    FIX ReadingTime : comptage même en option affichage de la description

--- ReadingTime/readingtime.plugin.disabled.php
+++ ReadingTime/readingtime.plugin.disabled.php
@@ -4,13 +4,19 @@
 @author Qwerty <qwerty@legtux.org>
 @link http://etudiant-libre.fr.nf
 @licence Tea Licence
-@version 1.0.0
+@version 1.0.1
 @description Estime le temps de lecture d'un article
 */
 
 function readingtime(&$event) {
+    $configurationManager = new Configuration();
+    $conf = $configurationManager->getAll();
+
+    $filtered_content = ($configurationManager->get('articleDisplayMode') == 'summary') ? $event->getDescription() : $event->getContent();
+
+    if( $filtered_content ) {
 		$readingtime =null;
-	    $word = str_word_count(strip_tags($event->getContent()));
+	    $word = str_word_count(strip_tags($filtered_content));
         $minutes_estimate = floor($word / 200);
         $seconds_estimate = floor($word / (200 / 60));
 		$hours = intval($seconds_estimate / 3600);
@@ -20,6 +26,7 @@ function readingtime(&$event) {
 		if($minutes>=1) { $readingtime .= $minutes.'min ';}
 		if($minutes<1 && $hours == 0) { $readingtime .= $secondes.'s ';}
 		echo _t('READING_TIME').' '.$readingtime;
+    }
 }
 
 Plugin::addHook("event_pre_top_options", "readingtime");  
